07b2d2b05e7f203b62c0f512c273fbfdd6fe9589
MnsDobariya <dobariyamansi121@gmail.com>
2023-12-30 19:09:51 +0530

    add comment

--- src/routes/v1/todos.route.js
+++ src/routes/v1/todos.route.js
@@ -5,7 +5,7 @@ const catchAsync = require('../../utils/catchAsync');
 const auth = require('../../middlewares/auth');
 
 const router = express.Router();
-
+// todo routes
 router.post('/create',auth(),validate(todosController.createToDos.validation),catchAsync(todosController.createToDos.handler));
 router.put('/update/:id',validate(todosController.updateToDos.validation),catchAsync(todosController.updateToDos.handler));
 router.delete('/delete/:id',catchAsync(todosController.deleteToDos.handler));
